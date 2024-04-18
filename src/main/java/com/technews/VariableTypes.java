7fa7e6c4f7c9fbf24090f231be01464b47ae0e63
Zachary Roy <vgd199@gmail.com>
2024-04-18 14:19:15 -0700

    added the structure of the database

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,16 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args){
+        int a = 15;
+        double b = 12.3;
+        String c = "cat";
+
+        double sum = a + b;
+
+        String concat = a + c;
+
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+}
